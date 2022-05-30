0eb5bf7ffd00ed62e2148b15b8189142b1d0584b
Srinivas-Bhat <srinivaskbhat840@gmail.com>
2022-05-30 16:36:20 +0530

    React evaluation 2

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -17,14 +17,14 @@ const AddProduct = () => {
         <ModalBody pb={6}>
           <Input data-cy="add-product-title" />
           <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+            <option data-cy="add-product-category-shirt">Shirt</option>
+            <option data-cy="add-product-category-pant">Pant</option>
+            <option data-cy="add-product-category-jeans">jeans</option>
           </Select>
           <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
+            <Radio data-cy="add-product-gender-male">Male</Radio>
+            <Radio data-cy="add-product-gender-female">Female</Radio>
+            <Radio data-cy="add-product-gender-unisex">Unisex</Radio>
           </RadioGroup>
           <Input data-cy="add-product-price" />
           <Button data-cy="add-product-submit-button"></Button>
